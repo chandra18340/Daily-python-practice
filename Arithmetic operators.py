e9373bb0a913592f5bfa0f700216a56f4588eada
Chandra Sekhar Mitta <99310512+chandra18340@users.noreply.github.com>
2022-08-13 13:28:55 +0530

    Add files via upload

---
+++ Arithmetic operators.py
@@ -0,0 +1,37 @@
+from __future__ import division
+
+
+Num1 = int(input("Enter any number: "))
+Num2 = int(input("Enter any number: "))
+
+# Addition 
+Addition = Num1 + Num2
+print(Addition)
+
+# Subtraction 
+Subtraction = Num1 - Num2
+print(Subtraction)
+
+# Multipliction 
+Multiplication = Num1 * Num2
+print(Multiplication)
+
+# Division 
+Division = Num1 / Num2
+print(Division)
+
+# Floor Division
+Flo = Num1 // Num2
+print(Flo)
+
+# Modulus Operator 
+Mod = Num1 % Num2
+print(Mod)
+
+# Exponent
+Exp = Num1 ** Num2
+print(Exp)
+
+
+
+
