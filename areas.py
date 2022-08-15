081d1cb61f2809ca271a14fb4b536ac188d5fda5
Chandra Sekhar Mitta <99310512+chandra18340@users.noreply.github.com>
2022-08-15 21:51:35 +0530

    Add files via upload

---
+++ areas.py
@@ -0,0 +1,19 @@
+from turtle import width
+
+
+# Area of rectangle
+length = int(input("lenght: "))
+width = int(input("width: "))
+area = length * width
+print("Area of rectangle is: ",area)
+
+# Area of triangle
+len = int(input("len: "))
+bre = int(input("bre: "))
+area2 = 0.5 * len * bre
+print("Area of the triangle is: ",area2)
+
+#Area of circle
+rad = int(input("radius = "))
+area3 = 3.14 * rad * rad
+print("Area of the circle is : ",area3)
