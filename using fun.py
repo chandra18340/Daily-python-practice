313d3fb14ab396c6b45520906f62d7c9c401a006
Chandra Sekhar Mitta <99310512+chandra18340@users.noreply.github.com>
2022-08-27 15:21:19 +0530

    Add files via upload

---
+++ using fun.py
@@ -0,0 +1,9 @@
+def add_sub_mul_div(x,y):
+    a = x + y
+    b = x - y
+    c = x * y
+    d = x / y
+    return a,b,c,d
+
+result1,result2,result3,result4 = add_sub_mul_div(5,6)
+print(result1 ,result2 ,result3 ,result4)
