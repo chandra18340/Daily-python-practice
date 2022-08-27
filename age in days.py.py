80efa03fa77bc8bd22fda52bb211c0ee23e41f70
Chandra Sekhar Mitta <99310512+chandra18340@users.noreply.github.com>
2022-08-27 19:07:04 +0530

    Add files via upload

---
+++ age in days.py.py
@@ -0,0 +1,11 @@
+def first_ele_in_list(num_list):
+    a = num_list[0]
+    return a
+result = first_ele_in_list([2 , 3, 5, 6])
+print(result)
+
+def human_age_in_days(age):
+    b = age * 365
+    return b
+result2 = human_age_in_days(70)
+print(result2)
