924f698aab6da05948d16a5e1cbde0ef8272afe1
Chandra Sekhar Mitta <99310512+chandra18340@users.noreply.github.com>
2022-08-28 19:38:04 +0530

    Add files via upload

---
+++ vechil.py
@@ -0,0 +1,8 @@
+class vehicle():
+    def __init__(self, speed, milage):
+        self.speed = speed
+        self.milage = milage
+modelA = vehicle(240, 20)
+print(modelA.speed, modelA.milage)
+
+        
