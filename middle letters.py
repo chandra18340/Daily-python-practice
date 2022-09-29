402607416f65d19ae2e723b47de7700c3082dd0a
Chandra Sekhar Mitta <99310512+chandra18340@users.noreply.github.com>
2022-09-29 20:54:36 +0530

    Add files via upload

---
+++ middle letters.py
@@ -0,0 +1,8 @@
+def mid(my_string):
+    length = len(my_string) % 2
+    if length == 0:
+        return ""
+    elif length != 0:
+        middle = len(my_string) // 2
+        return my_string[middle]
+print(mid("cars"))
